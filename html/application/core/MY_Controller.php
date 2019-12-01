93be244076f1d77450d0891c1fdacd10a235d3db
Wojciech Jeżowski <dirkuu@gmail.com>
2019-12-01 20:08:45 +0100

    MY_Controller with Home

---
+++ html/application/core/MY_Controller.php
@@ -0,0 +1,11 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class MY_Controller extends CI_Controller {
+	
+	private $title = 'Problemowanko xD';
+	
+	public function __construct() {
+		parent::__construct();
+	}
+}
