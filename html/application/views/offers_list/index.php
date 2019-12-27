3465a72c565adb91a1caee3fbae95a94e8926fc3
211954 Jeżowski Wojciech <211954@edu.p.lodz.pl>
2019-12-27 20:20:14 +0100

    Tabelka w divach

--- html/application/views/offers_list/index.php
+++ html/application/views/offers_list/index.php
@@ -1,8 +1,26 @@
 
+<div class='offers_table'>
 <?php
+
+echo "
+    <div class='offers_header row'>
+        <div class='offer_name'>Nazwa oferty</div>
+        <div class='offer_salary'>Pensja</div>
+        <div class='offer_company_name'>Nazwa firmy</div>
+        <div class='offer_city'>Miasto</div>
+    </div>";
+
 
 if (array_key_exists('offers', $main_content)) {
     foreach ($main_content['offers'] AS $offer) {
-        echo $offer['offer_name'] .' | '. $offer['salary'] .' | '. $offer['company_name'] .' | '. $offer['city'] .'<br />';
+        echo "
+            <div class='offer row'>
+                <div class='offer_name'>{$offer['offer_name']}</div>
+                <div class='offer_salary'>{$offer['salary']}</div>
+                <div class='offer_company_name'>{$offer['company_name']}</div>
+                <div class='offer_city'>{$offer['city']}</div>
+            </div>";
     }
 }
+?>
+</div>
