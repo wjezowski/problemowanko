5b61b4cae06f6c66e312276e74b7954d3ec74242
Wojciech Jeżowski <dirkuu@gmail.com>
2019-12-27 22:16:41 +0100

    Temp curl

--- html/application/views/offers_list/index.php
+++ html/application/views/offers_list/index.php
@@ -1,6 +1,26 @@
-
 <div class='offers_table'>
 <?php
+
+$url = 'localhost:8095/get.offers';
+
+
+
+$ch = curl_init($url);
+curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+    'Content-Type: application/json'
+));
+curl_setopt($ch, CURLOPT_TIMEOUT, 5);
+curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 5);
+
+//execute post
+$result = curl_exec($ch);
+var_dump(curl_error($ch));
+//close connection
+curl_close($ch);
+
+var_dump($result);
 
 echo "
     <div class='offers_header row'>
