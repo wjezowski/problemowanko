cb585100bc997dfd5eca3112d5420890085f27b1
Wojciech Jeżowski <dirkuu@gmail.com>
2019-12-27 22:15:05 +0100

    Improved update.sh by Marcin <3

--- update.sh
+++ update.sh
@@ -1,3 +1,12 @@
-rm -rf /home/wojtek/codeigniter/myapp/
-cp -r ./html/ /home/wojtek/codeigniter/myapp/
-docker restart codeigniter_myapp_1
+#!/bin/sh
+CURR_DIR=$(dirname "$(readlink -f "$0")")
+CODEIGNITER="$HOME/codeigniter"
+MY_APP="$CODEIGNITER/myapp"
+
+echo $MY_APP ytyyt myapp
+echo $CODEIGNITER ytyyt codeigniter
+echo $CURR_DIR dsfdsf curr_dir
+
+[ -e $MY_APP ] && rm -r $MY_APP
+cp -r $CURR_DIR/html $MY_APP
+cd $CODEIGNITER && docker-compose up --build --force-recreate ; cd -
