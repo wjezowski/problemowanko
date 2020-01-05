f84cde239048ae18c2c7ff4e991cd0c42a0ae9ae
Wojciech Jeżowski <dirkuu@gmail.com>
2020-01-05 21:21:53 +0100

    Improved update.sh by Marcin <3 x2

--- update.sh
+++ update.sh
@@ -3,10 +3,6 @@ CURR_DIR=$(dirname "$(readlink -f "$0")")
 CODEIGNITER="$HOME/codeigniter"
 MY_APP="$CODEIGNITER/myapp"
 
-echo $MY_APP ytyyt myapp
-echo $CODEIGNITER ytyyt codeigniter
-echo $CURR_DIR dsfdsf curr_dir
-
 [ -e $MY_APP ] && rm -r $MY_APP
 cp -r $CURR_DIR/html $MY_APP
-cd $CODEIGNITER && docker-compose up --build --force-recreate ; cd -
+cd $CODEIGNITER && docker-compose up -d --build --force-recreate ; cd -
